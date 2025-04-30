e6369e4988f2ad8f3ffdb31a75260a21f6b79c97
RebelStreak <richard@rebelliousstreak.com>
2025-04-30 16:41:45 +0000

    REmove dit and added wrangler.toml

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_b--0GUbH.mjs';
-import { manifest } from './manifest_D85kqGQW.mjs';
+import { manifest } from './manifest_CyjtGPLP.mjs';
 
 const serverIslandMap = new Map();;
 
